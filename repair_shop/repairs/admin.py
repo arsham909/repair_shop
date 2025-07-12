871286f9715e035fc18d8448eda7517defdac500
arsham <arsham202@gmail.com>
2025-07-12 10:38:21 +0200

    config and made repair jobs part

--- repair_shop/repairs/admin.py
+++ repair_shop/repairs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import RepairJobs
 # Register your models here.
+
+admin.site.register(RepairJobs)
--- repair_shop/repairs/models.py
+++ repair_shop/repairs/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.utils import timezone
 
 # Create your models here.
+class RepairJobs(models.Model):
+    class Status(models.TextChoices):
+        MBV = 'MBV', 'Must be validate'
+        Validated = 'Val', 'Validation'
+        Quoted = 'Quoted', 'Quoted'
+        WFP = 'WFP', 'Waiting for parts'
+        Done = 'Done', 'Done'
+        RTS = 'RTS' , 'Ready To Ship'
+        Shipped = 'Shipped', 'Shipped'
+        Scrap = 'Scrap', 'Scrapped'
+    
+    job_number = models.IntegerField(unique=True, help_text="Enter the job number:")
+    Device_name = models.CharField(max_length=250, verbose_name="Device brand:")
+    notes = models.CharField(max_length=250, help_text="notes")
+    device_part_number = models.IntegerField(
+        validators=[MaxLengthValidator(8), MinLengthValidator(6)],
+        help_text="Device Part number",
+        verbose_name="Device Part number",
+    )
+    can_test = models.BooleanField(verbose_name="can device be tested",)
+    assigned_to = models.CharField(max_length=250)
+    date = models.DateField(auto_now_add=True, verbose_name='date created',)
+    followed_up = models.CharField(max_length=10,
+                                choices=Status,default=Status.MBV)
+    status = models.CharField(max_length=250)
+    
+    video = models.FileField(upload_to='videos/',
+                            verbose_name='upload video',help_text='upload ur video here')
+    
+    parts_needs = models.CharField(max_length=250, help_text='parts needed ')
+    
+    
+    class Meta:
+        ordering = ['-date','-status']
+        
+        indexes = [
+            models.Index(fields=['-date'])
+        ]
