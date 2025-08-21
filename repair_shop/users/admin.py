8b94f567deb7fda40d577c9e7fd1a13e2092965e
arsham <arsham202@gmail.com>
2025-08-21 18:47:40 -0400

    added role to user admin

--- repair_shop/users/admin.py
+++ repair_shop/users/admin.py
@@ -6,4 +6,10 @@ from .models import Users
 #should use admin.ModelAdmin for model that you used abstract model in it 
 @admin.register(Users)
 class Users_account(UserAdmin):
-    list_display = ['username']
+    fieldsets = UserAdmin.fieldsets + (
+        ('additional info' , {'fields':('role',)}),
+        )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('additional info' , {'fields':('role',)}),
+    )
+    list_display = ['username', 'role']
