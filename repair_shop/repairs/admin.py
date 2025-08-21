d480dd91d44d7ca8707dbccf5fd3aa2d15df47ab
arsham <arsham202@gmail.com>
2025-08-21 18:44:29 -0400

    comment out the code for trying to write the models

--- repair_shop/repairs/admin.py
+++ repair_shop/repairs/admin.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
-from .models import RepairJobs , Company
+from .models import Repair , Company
 # Register your models here.
 
-class RepairInLine(admin.TabularInline):
-    model = RepairJobs
+# class RepairInLine(admin.TabularInline):
+#     model = RepairJobs
     
 
-@admin.register(RepairJobs)
-class RepairAdmin(admin.ModelAdmin):
-    list_display = ['job_number', 'device_name',
-                    'device_part_number', 'assigned_to',
-                    'parts_needs']
-    list_filter = ['status', 'date', 'assigned_to', 'brand']
-    search_fields = ['job_number', 'device_name',
-                    'device_part_number', 'assigned_to',
-                    'parts_needs', 'notes',
-                    'brand']
-    #date_hierarchy = ['date']
-    ordering = ['status', 'date']
-    #raw_id_fields = ['assigned_to']
+# @admin.register(RepairJobs)
+# class RepairAdmin(admin.ModelAdmin):
+#     list_display = ['job_number', 'device_name',
+#                     'device_part_number', 'assigned_to',
+#                     'parts_needs']
+#     list_filter = ['status', 'date', 'assigned_to', 'brand']
+#     search_fields = ['job_number', 'device_name',
+#                     'device_part_number', 'assigned_to',
+#                     'parts_needs', 'notes',
+#                     'brand']
+#     #date_hierarchy = ['date']
+#     ordering = ['status', 'date']
+#     #raw_id_fields = ['assigned_to']
     
-@admin.register(Company)
-class CompanyAdmin( admin.ModelAdmin):
-    list_display = ['name' , 'address' , 'phonenumber']
-    inlines = [RepairInLine]
+# @admin.register(Company)
+# class CompanyAdmin( admin.ModelAdmin):
+#     list_display = ['name' , 'address' , 'phonenumber']
+#     inlines = [RepairInLine]
