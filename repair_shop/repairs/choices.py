0dcb8d817d4329a802faf1bb8245fffe68a22dbf
arsham <arsham202@gmail.com>
2025-08-21 18:42:35 -0400

    added chice.py for better reading

---
+++ repair_shop/repairs/choices.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class State(models.TextChoices):
+    RECEIVED = "received", "Received"
+    VALIDATED = "validated", "Validated"
+    ASSIGNED = "assigned", "Assigned"
+    EVALUATING = "evaluating", "Evaluating"
+    QUOTED = "quoted", "Quoted"
+    APPROVED = "approved", "Approved"
+    REPAIRING = "repairing", "Repairing"
+    WAITING_FOR_PART = "waiting_for_part", "Waiting for part"
+    PART_ADDED = 'part_added', 'Part added'
+    SHIPPED = "shipped", "Shipped"
+    REJECTED = "rejected", "Rejected"
+    CANCELLED = "cancelled", "Cancelled"
+    SCRAP = 'scrap', 'Scrapped'
+    READY_TO_SHIP = 'ready_to_ship' , 'Ready to ship'
+    DONE = 'done', 'Done'
+    
+class CustomerRespond(models.TextChoices):
+    APPROVED = 'approved', "Approved"
+    REJECTED = 'rejected', "Rejected"
